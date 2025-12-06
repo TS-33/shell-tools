0934436f177cf0da7f23d72af0c7ccab0f33e070
thcsip <18328538244@163.com>
2025-12-06 17:14:46 +0800

    add var PASSWD

--- push_image.sh
+++ push_image.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 set -e
 
-usage='这个脚本用于运行docker build命令，生成一个基于当前目录下Dockerfile的image，并将生成的image推送到192.168.36.160、161、162节点，然后在节点上执行nerdctl -n k8s.io load <image> 命令，导入镜像。
+usage='这个脚本用于运行docker build命令，
+基于当前目录下Dockerfile的image，并将生成的image推送到给定的节点，
+最后在节点上执行nerdctl -n k8s.io load <image> 命令，导入镜像。
 '
+PASSWD='huawei@123'
 
 echo $usage
-echo 
+echo 默认密码: $PASSWD
 echo
 
-echo -n "Please input image's tag: "
-read TAG
+read -e -p "Input image's tag: " TAG
+read -e -p "Input target nodes: (like node1 node2) " NODES
 
 if [[ -z $TAG ]];then
 	echo Input can\'t be NULL!!
@@ -23,12 +26,19 @@ fi
 
 IMAGE_NAME=$(grep FROM Dockerfile | awk '{print $2}' | cut -d ':' -f 1)
 
-docker build -t ${IMAGE_NAME}:${TAG} .
-docker save ${IMAGE_NAME}:${TAG} -o ${IMAGE_NAME}_${TAG}.tar
+if [[ -f ${IMAGE_NAME}_${TAG}.tar ]];then
+    echo find ${IMAGE_NAME}_${TAG}.tar, I\'ll push this file directly...
+else
+	echo building image...
+	echo 
+	docker build -t ${IMAGE_NAME}:${TAG} .
+	docker save ${IMAGE_NAME}:${TAG} -o ${IMAGE_NAME}_${TAG}.tar
+fi
 
-for i in 160 161 162;do
-scp ${IMAGE_NAME}_${TAG}.tar 192.168.36.$i:/tmp
-ssh 192.168.36.$i "nerdctl -n k8s.io load -i /tmp/${IMAGE_NAME}_${TAG}.tar"
+
+for i in ${NODES};do
+sshpass -p $PASSWD scp ${IMAGE_NAME}_${TAG}.tar $i:/tmp
+sshpass -p $PASSWD ssh $i "nerdctl -n k8s.io load -i /tmp/${IMAGE_NAME}_${TAG}.tar"
 done
 
 

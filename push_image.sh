feb2e3513676f694053560d07af7b7e65878a75f
thcsip <18328538244@163.com>
2025-12-06 18:25:42 +0800

    Added cute tips ;)

--- push_image.sh
+++ push_image.sh
@@ -2,9 +2,11 @@
 set -e
 
 usage='这个脚本用于运行docker build命令，
-基于当前目录下Dockerfile的image，并将生成的image推送到给定的节点，
+基于当前目录下Dockerfile生成image，并推送到给定的节点，
 最后在节点上执行nerdctl -n k8s.io load <image> 命令，导入镜像。
 '
+
+
 PASSWD='huawei@123'
 
 echo $usage
@@ -12,7 +14,7 @@ echo 默认密码: $PASSWD
 echo
 
 read -e -p "Input image's tag: " TAG
-read -e -p "Input target nodes: (like node1 node2) " NODES
+read -e -p "Input target nodes(like node1 node2): " NODES
 
 if [[ -z $TAG ]];then
 	echo Input can\'t be NULL!!
@@ -37,6 +39,7 @@ fi
 
 
 for i in ${NODES};do
+echo transfer ${IMAGE_NAME}_${TAG}.tar to $i ...
 sshpass -p $PASSWD scp ${IMAGE_NAME}_${TAG}.tar $i:/tmp
 sshpass -p $PASSWD ssh $i "nerdctl -n k8s.io load -i /tmp/${IMAGE_NAME}_${TAG}.tar"
 done
